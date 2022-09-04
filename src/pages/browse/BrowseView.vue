342e0cbde5ab5a964df71fdd37d33201c6dd0a38
Emmanuel Garcia <earturoga@gmail.com>
2022-09-04 08:59:50 -0600

    completed nbrowse nav setup

--- src/pages/browse/BrowseView.vue
+++ src/pages/browse/BrowseView.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="browseDiv">
+    <TheBrowseNav></TheBrowseNav>
     <h1>Currently working on it....</h1>
   </div>
 </template>
 
 <script>
-export default {};
+import TheBrowseNav from "./TheBrowseNav.vue";
+export default {
+  components: {
+    TheBrowseNav,
+  },
+};
 </script>
 
 <style scoped>
@@ -13,9 +19,6 @@ export default {};
   min-height: 100vh;
   background-color: #141414;
   /* temporary */
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  position: relative;
 }
 </style>
---
+++ src/pages/browse/TheBrowseNav.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="content">
+    <div class="imgDiv">
+      <div>
+        <img :src="logo" alt="netflix logo" />
+      </div>
+    </div>
+    <div class="navBar">
+      <ul>
+        <li>Home</li>
+        <li>Account</li>
+        <li>Image</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import logo from "../../assets/netflix.png";
+export default {
+  data() {
+    return {
+      logo,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.content {
+  min-height: 8vh;
+  display: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 12% auto 30%;
+  grid-template-rows: 1pt;
+}
+.navBar {
+  grid-column: 3 / span 1;
+  width: 100%;
+  height: 8vh;
+}
+.navBar li {
+  list-style: none;
+  flex: 1;
+  text-align: center;
+}
+.navBar ul {
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  justify-content: space-evenly;
+}
+.imgDiv {
+  grid-column: 1 / span 1;
+  display: flex;
+  justify-content: center;
+}
+.imgDiv div {
+  height: 100%;
+}
+.imgDiv img {
+  width: 140px;
+  height: auto;
+  margin-top: -2vh;
+}
+@media only screen and (max-width: 700px) {
+  .content {
+    display: grid;
+    grid-template-columns: 25% auto;
+    grid-template-rows: 1pt;
+  }
+  .imgDiv img {
+    width: 100px;
+    height: auto;
+    margin-top: -5px;
+  }
+  .navBar {
+    grid-column: 2 / span 1;
+  }
+}
+</style>
