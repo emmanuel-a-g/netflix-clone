cbb883d5b727fe4df8ea24de44dc507d5995da1c
Emmanuel Garcia <earturoga@gmail.com>
2022-09-14 02:01:15 -0600

    add profile images

--- src/pages/profile/SelectProfile.vue
+++ src/pages/profile/SelectProfile.vue
@@ -4,15 +4,23 @@
     <div class="space"></div>
     <!-- SHOW CURRENT NAME AND PROFILE IMAGE -->
     <div class="currentDiv">
-      <div>
-        <h1>Name</h1>
-        <p>image</p>
+      <div class="header">
+        <div class="headerLeft">
+          <div class="icon">&#8592;</div>
+          <div>
+            <h1>Edit Profile</h1>
+            <h4>Choose a profile icon.</h4>
+          </div>
+        </div>
+        <div class="headerRight">
+          <p>EmmanuelG</p>
+          <img
+            src="https://res.cloudinary.com/milito/image/upload/v1663140262/profiles/classicTwo_et85sp.png"
+            alt="theImg"
+          />
+        </div>
       </div>
     </div>
-    <div>
-
-    </div>
-    <!-- SHOW THE OPTIONS -->
   </div>
 </template>
 
@@ -37,6 +45,7 @@ export default {
   position: relative;
   display: flex;
   flex-direction: column;
+  align-items: center;
 }
 .logo {
   position: absolute;
@@ -52,9 +61,51 @@ export default {
   width: 100%;
 }
 .currentDiv {
-  width: 100%;
+  width: 85%;
   height: 88vh;
-  background-color: aqua;
+  background-color: #141414;
   text-align: center;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.headerLeft {
+  color: white;
+  display: flex;
+  line-height: normal;
+}
+.icon {
+  font-size: 2.5rem;
+  margin: 0 5px;
+  font-weight: bolder;
+}
+.headerLeft h1 {
+  padding: 0;
+  margin: 0;
+  font-size: 2.4rem;
+  text-align: left;
+}
+.headerLeft h4 {
+  padding: 0;
+  margin: 0;
+  font-size: 1.7rem;
+  text-align: left;
+}
+.headerRight {
+  display: flex;
+  font-size: 1rem;
+}
+.headerRight p {
+  font-size: 1.6rem;
+  margin-right: 10px;
+  color: white;
+  font-weight: bold;
+}
+.headerRight img {
+  width: 100px;
+  height: auto;
+  border-radius: 5px;
 }
 </style>
